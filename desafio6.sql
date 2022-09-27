35c12955d523dd5f09396b01d7dc7e18e0df9718
BrenoOPrado <breno.o.prado@hotmail.com>
2022-09-26 23:03:30 -0300

    Requisito 6 e 7 iniciados :turtle:

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT MIN(pl.plan_value) AS "faturamento_minimo",
+MAX(pl.plan_value) AS "faturamento_maximo",
+ROUND(AVG(pl.plan_value), 2) AS "faturamento_medio"-- ,
+-- SUM(COUNT(us.plan_id) * pl.plan_value) AS "faturamento_total"
+FROM SpotifyClone.plans pl
+INNER JOIN SpotifyClone.user us
+ON pl.plan_id = us.plan_id;
+-- GROUP BY us.plan_id;
