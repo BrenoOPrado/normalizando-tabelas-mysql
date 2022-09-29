e6a255eb1956bd731146575d9f2b6095655bbf6a
BrenoOPrado <breno.o.prado@hotmail.com>
2022-09-29 12:40:38 -0300

    Requisito 4 corrigido :turtle:

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(music_id) AS "cancoes",
+SELECT COUNT(DISTINCT music_id) AS "cancoes",
 COUNT(DISTINCT artist_id) AS "artistas",
 COUNT(DISTINCT album_id) AS "albuns"
 FROM SpotifyClone.musics;
--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 SELECT us.name AS "usuario",
 IF(MAX(YEAR(his.reproduction_date)) >= 2021,
-'Usúario ativo', 'Usúario inativo') AS "status_usuario"
+'Usuário ativo', 'Usuário inativo') AS "status_usuario"
 FROM SpotifyClone.user us
 LEFT JOIN SpotifyClone.historic his
 ON his.user_id = us.user_id
